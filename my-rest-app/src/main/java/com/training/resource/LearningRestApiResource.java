c55cc8f399563097aa2f3e3c454536ad595e1b96
majrul <majrul@localhost>
2025-01-18 15:02:17 +0530

    updated code

--- my-rest-app/src/main/java/com/training/resource/LearningRestApiResource.java
+++ my-rest-app/src/main/java/com/training/resource/LearningRestApiResource.java
@@ -1,6 +1,7 @@
 package com.training.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -22,4 +23,19 @@ public class LearningRestApiResource {
 		
 		return "Response from example01";
 	}
+	
+	//http://localhost:8080/my-rest-app/api/example/02;empno=1001;name=John;salary=20000
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/02")
+	public String example02(
+			@MatrixParam("empno") int empno,
+			@MatrixParam("name") String name,
+			@MatrixParam("salary") double salary) {
+		
+		System.out.println(empno + " , " + name + " , " + salary);
+		
+		return "Response from example02";
+	}
+
 }
