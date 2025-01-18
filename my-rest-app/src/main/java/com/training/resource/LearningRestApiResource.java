7e28febddc0dcb4b538e721500ee540f21e176c7
majrul <majrul@localhost>
2025-01-18 16:27:41 +0530

    updated code with usage of beanparam

--- my-rest-app/src/main/java/com/training/resource/LearningRestApiResource.java
+++ my-rest-app/src/main/java/com/training/resource/LearningRestApiResource.java
@@ -2,7 +2,9 @@ package com.training.resource;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -92,4 +94,81 @@ public class LearningRestApiResource {
 	}
 
 
+	//http://localhost:8080/my-rest-app/api/example/06/abc/xyz;param4=123?param3=456
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/06/{param1}/{param2}")
+	public String example06(@BeanParam MyBean myBean) {
+			
+		System.out.println(myBean);
+		return "Response from example06";
+	}
+}
+
+
+class MyBean {
+	
+	@PathParam("param1")
+	private String param1;
+	
+	@PathParam("param2")
+	private String param2;
+	
+	@QueryParam("param3")
+	private String param3;
+	
+	@MatrixParam("param4")
+	private String param4;
+	
+	@HeaderParam("app-key")
+	private String appKey;
+
+	public String getParam1() {
+		return param1;
+	}
+
+	public void setParam1(String param1) {
+		this.param1 = param1;
+	}
+
+	public String getParam2() {
+		return param2;
+	}
+
+	public void setParam2(String param2) {
+		this.param2 = param2;
+	}
+
+	public String getParam3() {
+		return param3;
+	}
+
+	public void setParam3(String param3) {
+		this.param3 = param3;
+	}
+
+	public String getParam4() {
+		return param4;
+	}
+
+	public void setParam4(String param4) {
+		this.param4 = param4;
+	}
+
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+
+	@Override
+	public String toString() {
+		return "MyBean [param1=" + param1 + ", param2=" + param2 + ", param3=" + param3 + ", param4=" + param4
+				+ ", appKey=" + appKey + "]";
+	}
+
+	
+	
 }
