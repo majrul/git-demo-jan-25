8ca924a85b3916a7b9097cea11431e9c9fc3067a
majrul <majrul@localhost>
2025-01-20 20:45:19 +0530

    required file

---
+++ my-rest-app/src/main/java/com/training/model/ProductDetails.java
@@ -0,0 +1,31 @@
+package com.training.model;
+
+public class ProductDetails {
+
+	private int id;
+	private String propertyName;
+	private String propertyValue;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPropertyName() {
+		return propertyName;
+	}
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	public String getPropertyValue() {
+		return propertyValue;
+	}
+	public void setPropertyValue(String propertyValue) {
+		this.propertyValue = propertyValue;
+	}
+	
+	//private Map<String, Object> properties;
+
+	
+}
