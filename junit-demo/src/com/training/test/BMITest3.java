a1d4975fa0c438b08a45edcdfca1bc163ef311c8
majrul <majrul@localhost>
2025-01-23 19:37:05 +0530

    small change

--- junit-demo/src/com/training/test/BMITest3.java
+++ junit-demo/src/com/training/test/BMITest3.java
@@ -2,8 +2,12 @@ package com.training.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.training.demo.BMI;
 import com.training.demo.BMICalculator;
@@ -12,7 +16,8 @@ import com.training.demo.Person;
 public class BMITest3 {
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/bmi.csv", numLinesToSkip = 1)
+	//@CsvFileSource(resources = "/bmi.csv", numLinesToSkip = 1)
+	@MethodSource("provideData")
 	public void calculateBMI_FromCSVFileInput_ShouldReturnExpectedValue(double weight, double height, double expected) {
 		BMI bmi = new BMICalculator();
 		
@@ -24,6 +29,14 @@ public class BMITest3 {
 		
 		//then
 		assertEquals(expected, actual);
+	}
+	
+	
+	public static Stream<Arguments> provideData() {
+		return Stream.of(
+				Arguments.of(70, 1.7, 24),
+				Arguments.of(90, 1.7, 31),
+				Arguments.of(120, 1.7, 42));
 	}
 	
 }
